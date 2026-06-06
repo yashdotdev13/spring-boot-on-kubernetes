97624b9ca28ad14ff936942889f03b104a41240c
98001yash <yashchauhan.gaya@gmail.com>
2026-06-06 16:39:07 +0530

    dockerized the simple spring boot app without multi staging

--- 01-containerization/spring-boot-app/src/main/java/com/yash/spring_boot_app/controller/HelloController.java
+++ 01-containerization/spring-boot-app/src/main/java/com/yash/spring_boot_app/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.yash.spring_boot_app.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +11,19 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
+    @Value("${app.message}")
+    private String message;
 
-    @PostMapping("/hello")
-    public Map<String, String> hello(){
-        return Map.of(
-                "message",
-                "Hello from spring boot"
-        );
+//    @PostMapping("/hello")
+//    public Map<String, String> hello(){
+//        return Map.of(
+//                "message",
+//                "Hello from spring boot"
+//        );
+//    }
+
+        @GetMapping("/hello")
+        public Map<String, String> hello() {
+            return Map.of("message", message);
+        }
     }
-}
