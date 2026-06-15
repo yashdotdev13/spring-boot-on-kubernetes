117822fa905861d1c95c355b876123cc2e5b72cb
98001yash <yashchauhan.gaya@gmail.com>
2026-06-15 17:05:09 +0530

    renamed the profile-configmao.yaml

--- 03-configuration-management/spring-boot-app/src/test/java/com/yash/spring_boot_app/SpringBootAppApplicationTests.java
+++ 03-configuration-management/spring-boot-app/src/test/java/com/yash/spring_boot_app/SpringBootAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.yash.spring_boot_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class SpringBootAppApplicationTests {
 
 	@Test
