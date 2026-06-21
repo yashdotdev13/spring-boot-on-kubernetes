ee058618111252a11a79e610ddd60dca4376f72c
98001yash <yashchauhan.gaya@gmail.com>
2026-06-21 13:15:22 +0530

    created the controller for the chapter=04

---
+++ 04-production-readiness/spring-boot-app/src/main/java/com/yash/spring_boot_app/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.yash.spring_boot_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot V1";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Spring Boot App Version 1.0";
+    }
+
+}
